1cbe4b77a2dc708ecb0a629d91935a5abcc19ef2
sxclij <sxclij@gmail.com>
2024-10-12 10:10:07 +0000

    add .clang-format

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define sxclm_src_capacity (1<<16)
+#define sxclm_src_capacity (1 << 16)
 #define sxclm_path "test/1/traning.txt"
 
 int main() {
     char src[sxclm_src_capacity];
 
-    int fd = open(sxclm_path , O_RDONLY);
+    int fd = open(sxclm_path, O_RDONLY);
     int src_n = read(fd, src, sizeof(src) - 1);
     src[src_n] = '\0';
     close(fd);
 
     write(1, src, src_n);
-    write(1,"\n", 1);
+    write(1, "\n", 1);
 
     return 0;
 }
